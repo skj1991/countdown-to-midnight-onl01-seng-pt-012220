b634a4dc8b24834616b4b92c022fb3c8b086b4d0
Stacy Johns <stacykjohns@icloud.com>
2020-01-24 15:31:09 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  sleep(n)
 end
